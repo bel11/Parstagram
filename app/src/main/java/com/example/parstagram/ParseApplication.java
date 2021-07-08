c374a7b4d8efd8d1321c9288e7949a6662302abc
Belinda Xi <belindax@fb.com>
2021-07-07 22:47:34 -0400

    user can post photos

--- app/src/main/java/com/example/parstagram/ParseApplication.java
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -3,12 +3,15 @@ package com.example.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("3rhML70pY2fGj6EJWjHXGpswMxDAYYbpXFKz5319")
                 .clientKey("4EXdRr0goug35sa7QBD27qDjMKwXBOEr0TwS7vYB")
